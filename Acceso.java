1e04ae1f0107c1b71c73053ad399d0084144d931
Fenand <garciaf4c@gmail.com>
2018-02-27 20:32:05 +0100

    modificacion de comentarios

--- Acceso.java
+++ Acceso.java
@@ -2,8 +2,8 @@
 /**
  * Write a description of class Acceso here.
  * 
- * @author (your name) 
- * @version (a version number or a date)
+ * @author (fernando) 
+ * @version (1.0)
  */
 public class Acceso
 {
@@ -26,7 +26,9 @@ public class Acceso
     }  
 
     /**
-     * devuelve hora
+     * Metodo getter para obtener la hora del log
+     * 
+     * @return devuelve un int con la hora del log
      */
     public int getHoraDeAcceso()
     {
