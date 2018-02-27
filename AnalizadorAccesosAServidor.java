f6ca0c895a6909ad67d0a0b4732e07f5c31711ae
Fenand <garciaf4c@gmail.com>
2018-02-27 20:58:58 +0100

    Añadido el metodo obtenerHoraMasAccesos  de la clase analali... , añadidos archivos log y  clase test añadida

--- AnalizadorAccesosAServidor.java
+++ AnalizadorAccesosAServidor.java
@@ -19,7 +19,9 @@ public class AnalizadorAccesosAServidor
      */
     public AnalizadorAccesosAServidor()
     {
+
         logServidor = new ArrayList<>();
+
     }
 
     /**
@@ -28,11 +30,12 @@ public class AnalizadorAccesosAServidor
      * Este método debe leer el archivo de log y analizarlo para que
      * luego podamos invocar el siguiente método.
      * 
-     * @param Nombre del archivo de log con extension.log ya incluida solo poner el nombre del archivo
+     * @param Nombre del archivo de log con extension.log 
      */
     public void  analizarArchivoDeLog(String nombreArchivoALeer){
+
         try {
-            File archivo = new File(nombreArchivoALeer + ".log");
+            File archivo = new File(nombreArchivoALeer + ".log" );
             Scanner sc = new Scanner(archivo);
             while (sc.hasNextLine()) {
                 String linea = sc.nextLine();
@@ -46,24 +49,43 @@ public class AnalizadorAccesosAServidor
         catch (FileNotFoundException e) {
             e.printStackTrace();
         }
+
     }
 
-    // /**
-    // * Tener un método llamado obtenerHoraMasAccesos que, a partir de los procesos hechos por el método anterior, 
-    // * encuentra la hora (solo la hora, sin tener en cuenta los minutos) a la que se producen más accesos al servidor. 
-    // * El método muestra por pantalla dicha hora y la devuelve. Si hay empate, devuelve la hora más alta. 
-    // * En caso de que se invoque este método sin haberse invocado el anterior el método informa 
-    // * por pantalla de que no tiene datos, devuelve -1 y no hace nada más.
-    // * 
-    // * @return devuelve un int con la hora con mas accesos al servidor si no existen horas devuelve -1
-    // */
-    // public int obtenerHoraMasAccesos(){
-    // int horaConMasAccesos = -1;
-    // int contadorDeHorasIguales = 0;
+    /**
+     * Tener un método llamado obtenerHoraMasAccesos que, a partir de los procesos hechos por el método anterior, 
+     * encuentra la hora (solo la hora, sin tener en cuenta los minutos) a la que se producen más accesos al servidor. 
+     * El método muestra por pantalla dicha hora y la devuelve. Si hay empate, devuelve la hora más alta. 
+     * En caso de que se invoque este método sin haberse invocado el anterior el método informa 
+     * por pantalla de que no tiene datos, devuelve -1 y no hace nada más.
+     * 
+     * @return devuelve un int con la hora con mas accesos al servidor si no existen horas devuelve -1
+     */
+    public int obtenerHoraMasAccesos(){
+        int horaConMasAccesos = -1;
+        int contadorDeHorasIguales = 0;
 
-    // if(logServidor.size() > 0){
+        if(logServidor.size() > 0){
+            for(int i = 0; i < 24; i++){
+                int horaNuevaGuardada = i;
+                int horaNuevaQueTieneMasAccesos =  0;
+                for(Acceso log : logServidor){
+                    if(log.getHoraDeAcceso() == horaNuevaGuardada){
+                        horaNuevaQueTieneMasAccesos++;
+                    }
+                }
 
-    // }
-    // return horaConMasAccesos;
-    // }
+                if(horaNuevaQueTieneMasAccesos > contadorDeHorasIguales){
+                    contadorDeHorasIguales = horaNuevaQueTieneMasAccesos;
+                    horaConMasAccesos = horaNuevaGuardada;
+                }else if(horaNuevaQueTieneMasAccesos == contadorDeHorasIguales && horaConMasAccesos < horaNuevaGuardada){
+                    horaConMasAccesos = horaNuevaGuardada;
+                }
+            }
+            logServidor.clear();
+        }
+
+        return horaConMasAccesos;
+
+    }
 }
